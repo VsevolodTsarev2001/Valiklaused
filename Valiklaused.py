9773e212af2ff8de9a3540a42afd33e492e987cd
seva tsarev <seva.tsarev@gmail.com>
2024-01-19 00:33:52 +0200

    kodutöö

--- Valiklaused.py
+++ Valiklaused.py
@@ -57,3 +57,106 @@ hilinemine=input("Kas õpilane on hilinenud?")
 if hilinemine.capitalize()=="Jah":
     print("Õpilane ootab 30 min")
 print("Õpilane astub klassi")
+
+
+
+#2
+# import random
+# nimed = choice(["Kirill", "Bogdan", "Seva", "Gleb", "Martin", "Lev", "Sasha", "Elena", "Eva", "Mike", "Oleg", "Gena"])
+# valitud_nimed = random.sample(nimed, len(nimed))
+#
+#
+#
+# print(f"Täna sinu pinginaaber on {nimed}!")
+
+
+#3
+# arv1 = int(input("Sisestage ruumi esimese seina pikkus (sentimeetrites): "))
+# arv2 = int(input("Sisestage ruumi teise seina pikkus (sentimeetrites): "))
+#
+# S = arv1 * arv2
+# P = 2*(arv1 + arv2)
+# print("Периметр комнаты =", round(S, 2),"\n Ja ruumi pindala =", round(P, 2))
+#
+# remont = input("Kas soovite renoveerida? ")
+# if remont.capitalize() == "Ja":
+#     print(input("Kui palju maksab põranda asendamine ruutmeetri kohta?? "))
+
+#4
+# alghind = randint(0, 1000)
+# print(alghind)
+# if alghind >= 700:
+#     soodus_protsentides = 30
+#     allahindlus = (soodus_protsentides/ 100) * 30
+#     uus_hind = alghind - allahindlus
+#     print(f"Uus hind pärast 30% allahindlust on: {uus_hind}", )
+# else:
+#     print("Tootesoodustus algab 700€-st, seega 30% allahindlust ei saa")
+
+
+#5
+# temperatuur = randint(1, 20)
+# print(temperatuur)
+# if temperatuur >= 18:
+#     print("Temperatuur on üle 18 kraadi, mis on soovitav toasoojus talvel.")
+# else:
+#     print("Temperatuur on 18 kraadi või madalam, soovitav on hoida soojem temperatuur talvel.")
+
+
+
+
+
+#6
+# pikkus= int(input("Mis on sinu pikkus?: "))
+# print("Sa oled", pikkus)
+# if 150 <= pikkus < 168:
+#     pikkasvu = "lühike"
+# elif 169 <= pikkus < 177:
+#     pikkavu = "keskmine"
+# else:
+#     pikkasvu = "pikk"
+# print(f'Sa oled {pikkasvu}')
+            
+#7
+# pikkus = randint(140, 210)
+# print(pikkus)
+# sugu = choice(['mees', 'näine'])
+# print(sugu)
+# if sugu.lower() == 'mees':
+#     if pikkus < 165:
+#         print("Sa oled lühike mees.")
+#     elif 165 <= pikkus < 180:
+#         print("Sa oled keskmise pikkusega mees.")
+#     else:
+#         print("Sa oled pikk mees.")
+# elif sugu.lower() == 'naine':
+#     if pikkus < 155:
+#         print("Sa oled lühike naine.")
+#     elif 155 <= pikkus < 170:
+#         print("Sa oled keskmise pikkusega naine.")
+#     else:
+#         print("Sa oled pikk naine.")
+# else:
+#     print("Palun sisestage kehtiv sugu.")
+           
+#9
+# kylg1 = int(input("Sisestage kujundi esimene külg (cm): "))
+# kylg2 = int(input("sisestage kujundi teine külg (cm): "))
+# if kylg1 == kylg2:
+#     print("See on ruut!")
+# else:
+#     print("See ei ole ruut")
+         
+#11
+# import datetime
+#
+# birthday = input("Sisestage oma sünnipäev (YYYY-MM-DD): ")
+#
+# birthday = datetime.datetime.strptime(birthday, "%Y.%m.%d")
+# current_date = datetime.datetime.now()
+# age = current_date.year - birthday.year
+#
+# if age % 10 == 0:
+#     print("teie sünnipäev on tähtpäev")
+# else:
+#     print("teie sünnipäev ei ole tähtpäev")
