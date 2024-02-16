36a2a7aae3812099139aa28d5dbb27cac3d3ff2b
opilane <opilane@HP-CZC2349HTY>
2024-02-16 10:51:27 +0200

    uus comandid

--- lists/Praktiline_iseseisevtöö_listid/Praktiline_iseseisevtöö_listid.py
+++ lists/Praktiline_iseseisevtöö_listid/Praktiline_iseseisevtöö_listid.py
@@ -1,5 +1,7 @@
+from re import A
 from string import * 
 from random import *
+from time import sleep
 
 
 ##1
@@ -83,23 +85,122 @@ from random import *
 
 
 
-#4
-Indeksid=["Tallinn","Narva, Narva-Jõesuu","Kohtla-Järve","Ida-Virumaa, Lääne-Virumaa, Jõgevamaa","Tartu linn","Tartumaa, Põlvamaa, Võrumaa, Valgamaa","Viljandimaa, Järvamaa, Harjumaa, Raplamaa","Pärnumaa","Läänemaa, Hiiumaa, Saaremaa"]
+##4
+#Indeksid=["Tallinn","Narva, Narva-Jõesuu","Kohtla-Järve","Ida-Virumaa, Lääne-Virumaa, Jõgevamaa","Tartu linn","Tartumaa, Põlvamaa, Võrumaa, Valgamaa","Viljandimaa, Järvamaa, Harjumaa, Raplamaa","Pärnumaa","Läänemaa, Hiiumaa, Saaremaa"]
+#while True:
+#    while True:
+#        try:
+#            indeks=int(input("Sisesta oma indeks: "))
+#            indeks_elemendide_arv=len(str(indeks)) 
+#            if indeks_elemendide_arv==5:
+#                print("5numbriline indeks ")
+#                break
+#            else:
+#                print("On vaja 5numbriline arv(indeks)")
+#        except:
+#            print("Vale andmetüüp!")
+#    #arv1=indeks//10000
+#    #print(arv1)
+#    symbolid=list(str(indeks))
+#    sym1=symbolid[0]
+#    print(sym1)
+#    print(f"Sa elad piirkonnas {Indeksid[int(symbolid[0])-1]}")
+
+##5
+#rida=[]
+#N=randint(2,25)
+#for i in range(N):
+#    rida.append(choise(ascii_uppercase))
+#print(rida)
+#kogus=int(input("Mitu elemendi vahetame oma vahel "))
+#if len(rida)//2>=kogus:
+#    for i in range(kogus):
+#        a=rida[i]
+#        rida[i]=rida[len(rida)-i-1]
+#        rida[len(rida)-1-i]=a
+#print(rida)
+
+##6
+#numbers= input("Введите числа через пробел: ").split()
+#numbers=list(map(int, numbers))
+#if not numbers:
+#    print("Ошибка: список чисел пуст.")
+#else:
+#    max_number=max(numbers)
+#numbers[numbers.index(max_number)]=max
+
+#9
 while True:
-    while True:
-        try:
-            indeks=int(input("Sisesta oma indeks: "))
-            indeks_elemendide_arv=len(str(indeks)) 
-            if indeks_elemendide_arv==5:
-                print("5numbriline indeks ")
-                break
-            else:
-                print("On vaja 5numbriline arv(indeks)")
-        except:
-            print("Vale andmetüüp!")
-    #arv1=indeks//10000
-    #print(arv1)
-    symbolid=list(str(indeks))
-    sym1=symbolid[0]
-    print(sym1)
-    print(f"Sa elad piirkonnas {Indeksid[int(symbolid[0])-1]}")
+    try:
+        name = input("Sisestage nimi: ").strip()
+        if not name.isalpha():
+            raise ValueError("Nimi võib sisaldada ainult tähti.")
+        break
+    except ValueError as e:
+        print(e)
+
+print(f"Tere, {name.capitalize()}!")
+
+vowels = 0
+consonants = 0
+for letter in name:
+    if letter.lower() in 'aeiouõäöü':
+        vowels += 1
+    else:
+        consonants += 1
+
+print(f"Tähtede arv: {len(name)}")
+print(f"Vokaalide arv: {vowels}")
+print(f"Konsonantide arv: {consonants}")
+
+unique_letters = sorted(set(name.lower()))
+print("Tähestiku järjestuses nimi:", ", ".join(unique_letters))
+
+
+
+#16
+# Järjend vastustega
+vastused = ["Jah, kindlasti!", "Jah!", "Võib-olla!", "Ei!"]
+
+print("Tere tulemast küsimuste ja vastuste programmi!")
+
+while True:
+    input("Küsige jah/ei küsimus: ")
+    vastus = choice(vastused)
+    print("Vastus:", vastus)
+    if input("Kas soovite jätkata (jah/ei)? ").lower() != "jah":
+        print("Aitäh ja head aega!")
+        break
+
+
+
+#18
+print("Tere tulemast vene ruletti.!")
+print("Revolveris on 6 padrunit, millest üks on tühi.")
+
+while True:
+    print("\nMe alustame uut mängu!")
+
+    # Создаем список с патронами (1 - патрон, 0 - пусто)
+    revolver = [0, 0, 0, 0, 0, 0]
+    # Вставляем случайным образом один патрон
+    revolver[randint(0, 5)] = 1
+
+    input("Trummi keeramiseks vajutage Enter...")
+
+    print("Trummi keerutamine...")
+    sleep(2)  # Задержка для эффекта ожидания
+
+    # Выбираем случайный патрон
+    bullet_position = randint(0, 5)
+    if revolver[bullet_position] == 1:
+        print("lask!")
+        print("Game over!")
+    else:
+        print("klick!")
+        print("Sa jäid ellu!")
+
+    play_again = input("Kas sa tahad uuesti mängida? (ja/ei): ").lower()
+    if play_again != "ja":
+        print("Aitäh mängimise eest! Kohtumiseni!")
+        break
